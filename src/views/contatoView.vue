004662526da27dcebdee7a284cdf6c7328316449
Jhonata Gabriel Canevare <152428631+ojhonata@users.noreply.github.com>
2025-05-27 15:58:58 -0300

    .

--- src/views/contatoView.vue
+++ src/views/contatoView.vue
@@ -3,7 +3,7 @@
 
 
     <section class="justif-center p-20 w-[1000px]">
-      <h1 class="font-bold text-6xl">Atuo na criação de projetos web e back-end.</h1>
+      <h1 class="font-bold text-6xl">Atuo na criação de projetos front-end e back-end.</h1>
 
       <h2 class="font-semibold text-3xl pt-10 pb-10">Também estou aberto a colaborar em projetos, participar de hackathons, contribuir com ideias e trocar experiências com outros desenvolvedores. Se você tem uma ideia ou precisa de ajuda em algo, é só chamar!</h2>
 
